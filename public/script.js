6ee0fd2c8c7efddfe536d94f50940cff62bdcf5c
Jigglybuff19 <jigglybuff61911@gmail.com>
2025-08-12 14:06:54 +0800

    4

---
+++ public/script.js
@@ -0,0 +1,7 @@
+let age=57;
+let maxAge = 200;
+let numPerDay = 5;
+
+ let totalRequired = (numPerDay * 365) * (maxAge - age);
+
+ console.log("You will need " + totalRequired + "To last you until the ripe old age of " + maxAge);
